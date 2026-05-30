2184f1700fb6bae1b24cba6a1b4dfcd3a85ca312
kunal89204 <kunalkhandelwal108@gmail.com>
2026-05-30 16:49:25 +0530

    google tags added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,6 +79,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KB7M5KBL');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google Analytics */}
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-XGFPCV10CX"></script>
